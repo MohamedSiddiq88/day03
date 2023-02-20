606f72446d650968da05b4094ce95da84a7ffa27
MohamedSiddiq88 <124970161+MohamedSiddiq88@users.noreply.github.com>
2023-02-20 15:03:22 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,42 @@
+//resume
+let resume={
+    "name":"Mohamed Siddiq",
+    "phone":"123456789",
+    "email":"sample@gamil.com",
+    "address":{houseno:13,areaname:"xxx",city:"yyy",district:"zzz",pincode:608301},
+    "objective":"To secure an entry level position where I can utilize my expertise in the field of technology, while contributing to the development of the organization.",
+    "ACADEMIC PROFILE":[
+        {cource:"BCA","Name of Institution":"zzz college","Percentage of Marks Obtained":68},
+        {cource:"xII","Name of Institution":"yyy school","Percentage of Marks Obtained":66},
+        {cource:"X","Name of Institution":"yyy school","Percentage of Marks Obtained":65}
+    ],
+    "WORKING EXPERIENCE":"5 years",
+    "Skils":"full stack development",
+    "PERSONAL STRENGTH":[
+        "Able to do work individually as well as in a team.",
+        "Willing to learn and hardworking.",
+        "Good analytical skills and logical skills.",
+        "Adaptability to changing the environment and trends."
+    ],
+    "Hobbies":"learning new things",
+    "PERSONAL PROFILE":{
+        "Date of Birth":"6th Oct 1995",
+        "Gender":"Male",
+        "Father’s Name":"M.Shaikh Abdul rauf",
+        "Nationality":"Indian",
+        "Marital Status":"Single",
+        "Linguistic Ability":["Tamil","Hindi","English"], 
+    },
+    "DECLARATION":"I hereby declare that all the statements made in the above application are true and correct to the best of my knowledge and belief.",
+}
+
+console.log("****************************************************************for loo");
+let krr=Object.keys(resume);
+//console.log(krr)
+for(let i=0;i<krr.length;i++){
+    console.log(krr[i]+":"+resume[krr[i]]);
+}
+ console.log("************************************************************for in loo");
+for(let a in resume){
+    console.log(a+":"+resume[a])
+}
